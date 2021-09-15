8c94c0f25dee4ef16d05305d751c672d7932e85a
A01752789 <69563637+A01752789@users.noreply.github.com>
2021-09-15 14:00:47 -0500

    Acomodo de imagenes

--- proyfin/animacionfinal.py
+++ proyfin/animacionfinal.py
@@ -12,12 +12,21 @@ pygame.display.set_caption("Animacion")
 
 #Asignacion de imagenes a variables
 granjero = pygame.image.load("granjero.png")
+granjero = pygame.transform.scale(granjero, (75, 100))
 zorro = pygame.image.load("zorro.png")
+zorro = pygame.transform.scale(zorro, (75, 75))
 maiz = pygame.image.load("maiz.png")
+maiz = pygame.transform.scale(maiz, (75, 75))
 barco = pygame.image.load("bote.png")
+barco = pygame.transform.scale(barco, (150, 150))
 ganso = pygame.image.load("ganso.png")
+ganso = pygame.transform.scale(ganso, (75, 75))
 
-pantalla.blit(maiz,(5,10))
+pantalla.blit(granjero,(5,300))
+pantalla.blit(zorro,(5,230))
+pantalla.blit(maiz,(5,50))
+pantalla.blit(ganso,(5,150))
+pantalla.blit(barco,(100,350))
 #Bucle para solo cerrar la ventana al presionar el boton de cerrar
 while True:
   for event in pygame.event.get():
